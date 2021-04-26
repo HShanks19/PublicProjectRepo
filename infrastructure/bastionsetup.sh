735523a22185c9ceb7bdf083c5e6c36226f99b5e
holly-shanks <HShanks@qa.com>
2021-04-26 10:42:58 +0100

    IP Address change

--- infrastructure/bastionsetup.sh
+++ infrastructure/bastionsetup.sh
@@ -3,9 +3,7 @@ sudo apt-get update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
-sudo yum update -y
-sudo yum install git -y
-git clone https://github.com/HShanks19/BAE-sfia2-brief
+git clone https://github.com/HShanks19/PublicProjectRepo
 sudo apt install mysql-client-core-8.0
 # Code to run before snapshot created
 # mysql -h ${aws_db_instance.mysqldb.address} -P 3306 -u root -ppassword <<EOF
